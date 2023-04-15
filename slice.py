1f5dbe2658ebf10be5245e667db38270f448c1ee
AshishJadhav <vinodjadhav1845@gmail.com>
2023-04-15 13:56:50 +0530

    slice

---
+++ slice.py
@@ -0,0 +1,10 @@
+'''
+List Slices
+Write a program that takes a string as input and outputs the last character of that string.
+
+str = input()
+print(str[-1])
+
+'''
+names = "names"
+print(names[1:-1])
