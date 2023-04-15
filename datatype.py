64f0d03327315465667f45c4a9eb4208b929e876
AshishJadhav <vinodjadhav1845@gmail.com>
2023-04-15 13:26:43 +0530

    ash

---
+++ datatype.py
@@ -0,0 +1,39 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+'''
+Input Format
+
+The first line contains an integer that you must sum with .
+The second line contains a double that you must sum with .
+The third line contains a string that you must concatenate with .
+
+Output Format
+
+Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated strings on the third line.
+
+Sample Input
+
+12
+4.0
+is the best place to learn and practice coding!
+Sample Output
+
+16
+8.0
+HackerRank is the best place to learn and practice coding!
+
+'''
+
+i = 4
+d = 4.0
+s = "HackerRank"
+
+sum = int(input())
+sum2 = double(input())
+string = str(input())
+
+print(i + sum )
+print(d + sum2)
+print(s + string)
